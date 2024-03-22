59e53330ce872854a54be30bdcdd5f21a4d569c4
Federico Rodriguez <federico.rodriguez@intive.com>
2024-03-22 19:37:39 -0300

    axios added, response typed

--- src/bases/03-classes.ts
+++ src/bases/03-classes.ts
@@ -1,3 +1,8 @@
+import axios from "axios";
+import {
+  Move,
+  PokeapiResponse,
+} from "../interfaces/pokeapi-response.interface";
 //Clase es una representacion de un objeto real
 
 // Version larga
@@ -21,8 +26,31 @@ export class Pokemon {
   }
 
   constructor(public readonly id: number, public name: string) {}
+
+  // todo lo que no privado, es publico por defecto
+  // private no me deja acceder si esta por fuera de la clase
+  scream() {
+    console.log(`${this.name.toUpperCase()}!!!`);
+  }
+
+  speak() {
+    console.log(`${this.name}, ${this.name}`);
+  }
+
+  async getMoves(): Promise<Move[]> {
+    // const moves = 10
+    const { data } = await axios.get<PokeapiResponse>(
+      "https://pokeapi.co/api/v2/pokemon/4"
+    );
+
+    return data.moves;
+  }
 }
 
 export const charmander = new Pokemon(2, "Charmander");
 
 console.log(charmander);
+// charmander.speak();
+// charmander.scream();
+
+console.log(await charmander.getMoves());
