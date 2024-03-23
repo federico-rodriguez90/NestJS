1bf1e439f0417413e9450a7cdc60000dabba2b2b
Federico Rodriguez <federico.rodriguez@intive.com>
2024-03-23 00:24:51 -0300

    deprecated decorator

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,8 @@
 // import { pikachu } from "./bases/02-objetcts";
 // import { charmander } from "./bases/03-classes";
 // import { charmander } from "./bases/04-injection";
-import { charmander } from "./bases/05-decorators";
+// import { charmander } from "./bases/05-decorators";
+import { charmander } from "./bases/06-decorators2";
 import "./style.css";
 
 const app = document.querySelector<HTMLDivElement>("#app")!;
