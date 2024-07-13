344a84c30e195774fb2ddbbe3c161ac460185d57
Federico Rodriguez <fede90.12@gmail.com>
2024-07-13 15:44:10 -0300

    end section 5

--- src/cars/cars.service.ts
+++ src/cars/cars.service.ts
@@ -6,23 +6,11 @@ import { CreateCarDto, UpdateCarDto } from './dto';
 @Injectable()
 export class CarsService {
   private cars: Car[] = [
-    {
-      id: uuid(),
-      brand: 'Toyota',
-      model: 'Corolla',
-    },
-
-    {
-      id: uuid(),
-      brand: 'Honda',
-      model: 'Civic',
-    },
-
-    {
-      id: uuid(),
-      brand: 'Ford',
-      model: 'Mustang',
-    },
+    // {
+    //   id: uuid(),
+    //   brand: 'Toyota',
+    //   model: 'Corolla',
+    // },
   ];
 
   findAll() {
@@ -74,5 +62,9 @@ export class CarsService {
     }
 
     this.cars.filter((car) => car.id !== id);
+  }
+
+  fillCarsWithSeedData(cars: Car[]) {
+    this.cars = cars;
   }
 }
